9515e1898136f0959713e75f413985fe5a7db17c
Ivan Klenov <ivanklenov@users.noreply.github.com>
2026-09-04 19:53:51 +0300

    fix(about): use new bold clear icon across all resolutions and about dialog

--- src/BuildNumber.h
+++ src/BuildNumber.h
@@ -1,5 +1,5 @@
 // Generated automatically by cmake/update_build_number.cmake
 #pragma once
 
-#define BUILD_NUMBER 96
-#define BUILD_NUMBER_STR "96"
+#define BUILD_NUMBER 97
+#define BUILD_NUMBER_STR "97"
